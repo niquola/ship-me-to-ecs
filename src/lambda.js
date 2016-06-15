5c4a73a1dfbf313845e8c1a81c2f458f281231d7
niquola <niquola@gmail.com>
2016-06-15 14:14:42 +0300

    Start promisification

--- src/lambda.js
+++ src/lambda.js
@@ -1,86 +1,80 @@
 "use strict";
 
 let AWS = require('aws-sdk');
-let proc = require('child_process');
 let fs = require('fs');
+let shell = require('./shell');
+let Promise = require("bluebird");
 
-function bash(cmd, cb){
-  console.log(cmd);
-  let buildProc = proc.exec("bash -c \"" + cmd + "\"");
-  buildProc.stdout.pipe(process.stdout);
-  buildProc.stderr.pipe(process.stderr);
-  buildProc.on('exit', function(code){
-    console.log("EXIT:", arguments);
-    if(code === 0){
-      cb();
-    }
-  });
-}
+let bash = shell.bash;
 
-function deploy(cfg, lambda){
-  var LAMBDA = new AWS.Lambda({
-    apiVersion: '2015-03-31',
-    region: cfg.region
-  });
-  console.log("+++++++++++++++++++++++++");
-  console.log("Deploy Lambda " + lambda.path);
-  console.log("+++++++++++++++++++++++++");
-  
-  process.chdir('./' + lambda.path);
-  bash('source ~/.nvm/nvm.sh && nvm install 4.3.2', function(res){
-    console.log("Node installed");
-    bash('source ~/.nvm/nvm.sh && nvm use 4.3.2 && npm install', function(res){
-      bash('rm -rf ../' + lambda.path + '.zip && zip -r ../' + lambda.path + '.zip ./*', function(res){
-        console.log('Package was zipped');
-        process.chdir('../');
-        var buffer = fs.readFileSync('./' + lambda.path + '.zip');
 
-        LAMBDA.getFunction({FunctionName: lambda.name}, function(err, data) {
-          if (err){
-            console.log(err);
-            console.log("CREATE");
-            var params = {
-              Code: { ZipFile: buffer },
-              FunctionName: lambda.name,
-              Handler: 'index.handler',
-              Role: lambda.role,
-              Runtime: 'nodejs4.3',
-              Description: lambda.description || 'TODO',
-              MemorySize: 300,
-              Publish: true,
-              Timeout: 100
-            };
-            LAMBDA.createFunction(params, function(err, data) {
-              if (err) {
-                console.log(err, err.stack); 
-              } else {
-                console.log("Lambda " + lambda.name + " was created", data);
-              }
-            });
-          }
-          else {
-            var params = {
-              ZipFile: buffer,
-              FunctionName: lambda.name,
-              Publish: true
-            };
-            LAMBDA.updateFunctionCode(params, function(err, data) {
-              if (err) {
-                console.log(err, err.stack); 
-              } else {
-                console.log("Lambda " + lambda.name + "was updated", data);
-              }
-            });
-          }
-        });
-      });
+function fnExists(LAMBDA, name){
+  return new Promise(function(resolve, reject){
+    LAMBDA.getFunction({FunctionName: name}, function(err, data){
+      if(err){ resolve(false);}
+      else resolve(true);
     });
   });
 }
+function deploy(cfg, lambda){
+  var LAMBDA = Promise.promisifyAll(new AWS.Lambda({
+    apiVersion: '2015-03-31',
+    region: cfg.region
+  }), {filter: function(name) {return name.indexOf('Async') < 0;}});
+
+  console.log(LAMBDA.getFunction);
+  console.log("+++++++++++++++++++++++++");
+  console.log("Deploy Lambda " + lambda.path);
+  console.log("+++++++++++++++++++++++++");
+
+  process.chdir('./' + lambda.path);
+
+  return bash('source ~/.nvm/nvm.sh && nvm install 4.3.2')
+    .then(function(res){
+      console.log("node was installed");
+      return bash('source ~/.nvm/nvm.sh && nvm use 4.3.2 && npm install');
+    }).then(function(res){
+      console.log("npm was installed");
+      return bash('rm -rf ../' + lambda.path + '.zip && zip -r ../' + lambda.path + '.zip ./*');
+    }).then(function(res){
+      console.log('Package was zipped');
+      process.chdir('../');
+      return fnExists(LAMBDA, lambda.name);
+    }).then(function(exists){
+      var buffer = fs.readFileSync('./' + lambda.path + '.zip');
+      if(exists){
+        console.log("Update lambda");
+        return LAMBDA.updateFunctionCodeAsync({
+            ZipFile: buffer,
+            FunctionName: lambda.name,
+            Publish: true
+          });
+      } else {
+        console.log("Create lambda");
+        return LAMBDA.createFunctionAsync({
+            Code: { ZipFile: buffer },
+            FunctionName: lambda.name,
+            Handler: 'index.handler',
+            Role: lambda.role,
+            Runtime: 'nodejs4.3',
+            Description: lambda.description || 'TODO',
+            MemorySize: 300,
+            Publish: true,
+            Timeout: 100
+          });
+      }
+    }).catch(function(err){
+      console.log("ERROR WHILE LAMBDA:", err);
+    }).then(function(res){
+      console.log("Lambda was updated/created", res);
+    });
+}
 
 function run(cfg){
-  cfg.lambdas.forEach(function(lambda){
-    deploy(cfg, lambda);
-  });
+  if(cfg.lambdas && cfg.lambdas.length > 0){
+    return deploy(cfg, cfg.lambdas[0]);
+  }else {
+    console.log("No lambdas found in config");
+  }
 }
 exports.run = run;
---
+++ src/shell.js
@@ -0,0 +1,22 @@
+"use strict";
+
+let proc = require('child_process');
+var Promise = require("bluebird");
+
+function bash(cmd, cb){
+  console.log("EXECUTE:", cmd);
+  return new Promise(function (resolve, reject) {
+    let buildProc = proc.exec("bash -c \"" + cmd + "\"");
+    buildProc.stdout.pipe(process.stdout);
+    buildProc.stderr.pipe(process.stderr);
+    buildProc.on('exit', function(code){
+      if(code === 0){
+        resolve(code);
+      }else {
+        reject(code);
+      }
+    });
+  });
+}
+
+exports.bash = bash;
